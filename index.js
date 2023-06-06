3cb45b5dd82d28b4b120dcdeda229a497b78cdd1
DuvanFelipeDeveloper <duvanfelipe1723@gmail.com>
2023-06-06 15:03:31 -0500

    Se agrego la configuracion de estilos y es planteo el funcionamiento de tooltips a la derecha,izquierda, abajo y arriba

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Tooltip = require('./src/tooltips');
 
 const button = document.querySelector('.my-button');
-const tooltip = new Tooltip(button, 'Haz clic aquí');
+const tooltip = new Tooltip(button, 'Haz clic aquiiiiiii','top');
 
 button.addEventListener('mouseenter', () => {
   tooltip.show();
--- src/tooltips.js
+++ src/tooltips.js
@@ -1,11 +1,25 @@
 // tooltips.js
 class Tooltip {
-    constructor(element, text) {
+    constructor(element, text, position) {
       this.element = element;
       this.text = text;
+      this.position = position;
       this.tooltipElement = document.createElement('div');
       this.tooltipElement.classList.add('tooltip');
       this.tooltipElement.innerText = this.text;
+      console.log(position)
+
+      if (this.position === 'right') {
+        this.tooltipElement.classList.add('tooltip-right');
+      } else if (this.position === 'left') {
+        this.tooltipElement.classList.add('tooltip-left');
+      } else if (this.position === 'top') {
+        this.tooltipElement.classList.add('tooltip-top');
+      } else if (this.position === 'bottom') {
+        this.tooltipElement.classList.add('tooltip-bottom');
+      }
+
+
     }
   
     show() {
